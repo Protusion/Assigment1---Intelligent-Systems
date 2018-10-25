27a12d44dd2b7560e79792c16b67f5d4a7bc89ad
ALBERTO SANCHEZ ORDONEZ <Alberto.Sanchez36@alu.uclm.es>
2018-10-25 15:07:28 +0200

    Breath First ADDED

--- Assignment1 - IS/src/search/BreathFirst.java
+++ Assignment1 - IS/src/search/BreathFirst.java
@@ -6,27 +6,59 @@
 package search;
 
 import java.util.ArrayList;
+import java.util.PriorityQueue;
+import problems.maze.MazeState;
 import search.*;
 
 /**
  *
  * @author Alberto
  */
-public class BreathFirst extends SearchAlgorithm{
+public class BreathFirst extends SearchAlgorithm {
 
+    protected ArrayList<State> explored = new ArrayList();
+    protected PriorityQueue open = new PriorityQueue(100, Node.BY_DEPTH);
+    
     @Override
     public void setParams(String[] params) {
-        /* NOTHING */
+        Node initialNode = new Node(super.problem.initialState());
+        open.add(initialNode);
     }
 
     @Override
     public void doSearch() {
-        ArrayList<Node> open = new ArrayList<Node>();
-        Node initialState = new Node(super.problem.initialState());
-        open.add(initialState);
-        while(!open.isEmpty()){
-            
+        super.generatedNodes = 1;
+        while (!open.isEmpty()) {
+            Node extractedNode = (Node) open.remove();
+            super.generatedNodes++;
+            if (!explored.contains(extractedNode.getState())) {
+                if (problem.testGoal(extractedNode.getState())) {
+                    super.actionSequence.add(extractedNode.getAction());
+                    Node parent = extractedNode.getParent();;
+                    
+                    for (State state : explored) {
+                        
+                        if (state.equals(parent.getState())) {
+                            super.totalCost+=problem.cost(state, parent.getAction());
+                            super.actionSequence.add(parent.getAction());
+                            parent = parent.getParent();
+                            explored.remove(state);
+                            super.exploredMaxSize--;
+                        }
+                        
+                    }
+                    
+                    super.solutionFound = true;
+                }
+                
+                explored.add(extractedNode.getState());
+                super.exploredMaxSize++;
+                open.add(super.getSuccessors(extractedNode));
+               super.expandedNodes++;
+
+            }
+
         }
     }
-    
+
 }
